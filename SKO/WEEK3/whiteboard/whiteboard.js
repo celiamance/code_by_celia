23cb7edfd3473727e9204088fd8089b438546282
celiamance <68975929+celiamance@users.noreply.github.com>
2020-10-21 15:18:08 +1100

    Add files via upload

---
+++ SKO/WEEK3/whiteboard/whiteboard.js
@@ -0,0 +1,15 @@
+function setup() {
+createCanvas(windowWidth, windowHeight);
+background(241,253,224);
+}
+
+
+function draw() {
+noStroke();
+fill(181,235,90,50);
+ellipse(mouseX, mouseY, 30, 30);
+}
+
+function mousePressed(){
+background(241,253,224);
+}
